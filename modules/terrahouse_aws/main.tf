ebfe21b43f0e6dfe0a463b059a6e12f67a761d16
Paulbasz Afolabi <paulbass84@gmail.com>
2023-10-25 11:53:55 +0000

    comment out code make things work

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -32,13 +32,13 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = var.index_html_filepath
+#resource "aws_s3_object" "index_html" {
+#  bucket = aws_s3_bucket.website_bucket.bucket
+#  key    = "index.html"
+ # source = var.index_html_filepath
 
-  etag = filemd5(var.index_html_filepath)
-}
+ # etag = filemd5(var.index_html_filepath)
+#}
 
 #resource "aws_s3_object" "upload_assets" {
  #for_each = fileset(var.assets_path,"*.{jpg,png,gif}")
@@ -50,11 +50,12 @@ resource "aws_s3_object" "index_html" {
 #}
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-resource "aws_s3_object" "error_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "error.html"
-  source = var.error_html_filepath
+#resource "aws_s3_object" "error_html" {
+ # bucket = aws_s3_bucket.website_bucket.bucket
+ # key    = "error.html"
+ # source = var.error_html_filepath
 
-  etag = filemd5(var.error_html_filepath)
-}
+  #etag = filemd5(var.error_html_filepath)
+#}
 
+data "aws_caller_identity" "current" {}
